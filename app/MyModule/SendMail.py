64b137d1dab87af8acaa3b04fc3ba9d547a432a7
koioschen <chjz1226@gmail.com>
2020-03-25 13:39:44 +0800

    test

--- app/MyModule/SendMail.py
+++ app/MyModule/SendMail.py
@@ -56,21 +56,18 @@ class sendmail:
         msg['BCc'] = self.BCC
         msg["Accept-Language"] = "zh-CN"
         msg["Accept-Charset"] = "ISO-8859-1,utf-8,gb2312"
-        try:
-            server = smtplib.SMTP(timeout=30)
-            server.connect(self.HOST, "25")
-            server.login(self.FROM, self.PASSWD)
-            send_msg = msg.as_string()
-            logger.info('************** mail content ***************\n')
-            logger.debug(f'{send_msg}')
-            to_ = self.TO if isinstance(self.TO, list) else [self.TO]
-            bcc_ = self.BCC if isinstance(self.BCC, list) else [self.BCC]
-            total_sent_to = to_ + bcc_
-            logger.debug(total_sent_to)
-            server.sendmail(self.FROM, to_ + bcc_, send_msg)
-            server.quit()
-            logger.info(f">>> It is success to send the mail to {str(total_sent_to)}!")
-            return True
-        except Exception as e:
-            logger.info(f">>> It is failed to send the mail for {e}!")
-            return False
+        server = smtplib.SMTP(timeout=30)
+        server.connect(self.HOST, "25")
+        server.login(self.FROM, self.PASSWD)
+        send_msg = msg.as_string()
+        logger.info('************** mail content ***************\n')
+        logger.debug(f'{send_msg}')
+        to_ = self.TO if isinstance(self.TO, list) else [self.TO]
+        bcc_ = self.BCC if isinstance(self.BCC, list) else [self.BCC]
+        total_sent_to = to_ + bcc_
+        logger.debug(total_sent_to)
+        server.sendmail(self.FROM, to_ + bcc_, send_msg)
+        server.quit()
+        logger.info(f">>> It is success to send the mail to {str(total_sent_to)}!")
+        return True
+
